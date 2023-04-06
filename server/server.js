9622d4830904834432ca2d9253a0d2910742ae49
kenbadertscher <badertscher.ken@gmail.com>
2023-04-06 19:56:53 -0400

    installed dependencies

--- server/server.js
+++ server/server.js
@@ -10,20 +10,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = proces.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-})
+        console.log("MongoDB database connection established successfully");
+    })
+    
+    const templateRouter = require('.routes/template');
+    const usersRouter = require('.routes/users');
+    
+    app.use('/templates', templatesRouter);
+    app.use('/users', usersRouter);
 
-const templatesRouter = require('.routes/templates');
-const usersRouter = require('.routes/users');
-
-app.use('/templates', templatesRouter);
-app.use('/users', usersRouter);
-
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});

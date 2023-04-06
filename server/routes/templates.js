820a0fe1bad7347da171247afee22af9ff7c7836
kenbadertscher <badertscher.ken@gmail.com>
2023-04-05 21:09:49 -0400

    commit at route tests

--- server/routes/templates.js
+++ server/routes/templates.js
@@ -24,3 +24,32 @@ router.route('/add').post((req, res) => {
     .then(() => res.json('Template added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
+
+router.route('/:id').get((req, res) => {
+    Template.findById(req.params.id)
+        .then(template => res.json(template))
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+router.route('/:id').delete((req, res) => {
+    Template.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Template deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+router.route('/update/:id').post((req, res) => {
+    Template.findById(req.params.id)
+        .then(template => {
+            template.username = req.body.username;
+            template.description = req.body.description;
+            template.duration = Number(req.body.duration);
+            template.date = Date.parse(req.body.date);
+
+            template.save()
+                .then(() => res.json('Template updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+module.exports = router;
